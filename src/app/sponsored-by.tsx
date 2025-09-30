919a3b19c8c36199c7c792df462572f362bff3d4
Miguel-Chaparro <miguel.chapariza@gmail.com>
2025-09-29 22:44:46 -0500

    add social media

--- src/app/sponsored-by.tsx
+++ src/app/sponsored-by.tsx
@@ -4,12 +4,15 @@ import Image from "next/image";
 import { Typography } from "@material-tailwind/react";
 
 const SPONSORS = [
-  "coinbase",
-  "spotify",
-  "pinterest",
-  "google",
-  "amazon",
-  "netflix",
+  "instagram",
+  "facebook",
+  "tiktok",
+];
+
+const SPONSORS_LINKS = [
+  "https://www.instagram.com/detoxcalifornia/",
+  "https://www.facebook.com/profile.php?id=61581107631177",
+  "https://www.tiktok.com/@detox.drinks",
 ];
 
 export function SponsoredBy() {
@@ -17,18 +20,24 @@ export function SponsoredBy() {
     <section className="py-8 px-8 lg:py-20">
       <div className="container mx-auto text-center">
         <h6 className="mb-8 text-blue-gray-700">
-          SPONSORED BY
+          Nuestras Redes
         </h6>
         <div className="flex flex-wrap items-center justify-center gap-6">
           {SPONSORS.map((logo, key) => (
+            <a
+              key={key}
+              href={SPONSORS_LINKS[key]}
+              target="_blank"
+            > 
             <Image
               width={256}
               height={256}
               key={key}
-              src={`/logos/logo-${logo}.svg`}
+              src={`/logos/logo-${logo}.png`}
               alt={logo}
               className="w-40"
             />
+            </a>
           ))}
         </div>
       </div>
